4ab55a1334e412524f2757dfb934368e4102f9bb
L2 <luclaurent@crans.org>
2019-04-30 16:01:01 +0200

    add case without any wall in cavity

--- Acoustic3D_Structure3D/Plot_frf_v3.py
+++ Acoustic3D_Structure3D/Plot_frf_v3.py
@@ -6,7 +6,8 @@ import scipy
 import pylab as pl
 import pickle
 
-f=open('results/cavity_acou3D_struc_3D_v3_results.frf','rb')
+#f=open('results/cavity_acou3D_struc_3D_v3_results.frf','rb')
+f=open('results/cavity_acou3D_struc_3D_v3_4.00E+00_3.00E+00_2.49E+00_1.00E+00_results.frf','rb')
 frf_0=pickle.load(f)
 f.close()
 
@@ -17,7 +18,7 @@ pl.plot(frf_0[0],10*log10(frf_0[1]/prefsquare),'r-',label='test 0', linewidth=1)
 #pl.axis([1.0, 120.0, 70, 105])
 pl.xlabel('Frequency (Hz)')
 pl.ylabel('Mean quadratic pressure (dB)')
-pl.grid('on')
+pl.grid(True)
 pl.legend(loc=4)
 
 pl.show()
--- Acoustic3D_Structure3D/run.sh
+++ Acoustic3D_Structure3D/run.sh
@@ -1,2 +1,2 @@
 export OMP_NUM_THREADS=1
-mpirun -np 20 python parametricStudyXY.py >> log.out 2>&1 &
+mpirun -np 20 python parametricStudyXYZ.py >> logXYZ.out 2>&1 &
