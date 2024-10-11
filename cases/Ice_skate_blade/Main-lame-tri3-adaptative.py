6ea9c693c64c5688fc6437a0724002ae1a8ae52f
Luc LAURENT <luc.laurent@cnam.fr>
2024-10-11 11:30:45 +0200

    add Ice skating balde case
    Former-commit-id: f0d8b7625dc9f1aadeb15f8eb4a1641980f1726a

---
+++ cases/Ice_skate_blade/Main-lame-tri3-adaptative.py
@@ -0,0 +1,258 @@
+#############################################################################
+#      Import libraries
+#############################################################################
+import string
+import time
+import scipy
+import scipy.sparse
+import scipy.sparse.linalg
+import sys
+sys.path.append('../../librairies')
+import silex_lib_gmsh
+
+#import silex_lib_tri3_python as silex_lib_elt
+import silex_lib_tri3_fortran as silex_lib_elt
+import silex_lib_extra_python as silex_lib_extra
+import os
+
+
+import mumps
+
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+nproc=comm.Get_size()
+rank = comm.Get_rank()
+
+class comm_mumps_one_proc:
+    rank = 0
+    def py2f(self):
+        return 0
+    
+mycomm=comm_mumps_one_proc()
+
+#############################################################################
+print("SILEX CODE - calcul d'un capteur de force avec des tri3")
+#############################################################################
+
+#############################################################################
+#      USER PART: Import mesh, boundary conditions and material
+#############################################################################
+tic = time.clock()
+
+# Input mesh: define the name of the mesh file (*.msh)
+MeshFileName='adaptive_mesh'
+#make_capteur_mesh.WriteGmshGeoTri3('initial_mesh',[0.0,0.0,10.0,10.0])
+
+Elt_max_length=10.0
+
+# Output result file: define the name of the result file (*.msh)
+ResultsFileName='Results_lame-tri3'
+
+# choose the element type
+eltype=2
+
+# choose geometry dimension
+ndim=2
+
+# choose the results in the results file
+flag_write_fields=0
+
+ErrorGlobal=0.99
+
+os.system('cp initial_mesh.msh adaptive_mesh.msh')
+
+Step_number=1
+
+ErrorGlobalMaxi = 0.03
+
+idnodes=scipy.zeros(3,dtype=int)
+a23=scipy.zeros((2,3),dtype=float)
+
+while (ErrorGlobal>ErrorGlobalMaxi):
+    print("---------------  STEP NUMBER  ---------------  ",Step_number)
+    # read the mesh from gmsh
+    nodes=silex_lib_gmsh.ReadGmshNodes(MeshFileName+'.msh',2)
+    elements,Idnodes=silex_lib_gmsh.ReadGmshElements(MeshFileName+'.msh',eltype,3)
+
+    # read surfaces where to impose boundary conditions
+    elementsS1,IdnodeS1=silex_lib_gmsh.ReadGmshElements(MeshFileName+'.msh',1,1)
+    elementsS2,IdnodeS2=silex_lib_gmsh.ReadGmshElements(MeshFileName+'.msh',1,2)
+
+    # write the surface mesh in a gmsh-format file to verify if its correct
+    #silex_lib_gmsh.WriteResults(ResultsFileName+'_Surface_mesh',nodes,elements,2)
+    #silex_lib_gmsh.WriteResults(ResultsFileName+'_S2_mesh',nodes,elementsS2,1)
+    #silex_lib_gmsh.WriteResults(ResultsFileName+'_S3_mesh',nodes,elementsS3,1)
+
+    # Define material
+    Young  = 200000.0
+    nu     = 0.3
+    thickness = 1.0
+
+    # define fixed dof
+    IdNodesFixed_x=IdnodeS1
+    IdNodesFixed_y=IdnodeS1
+
+    # force vector
+    F=silex_lib_elt.forceonline(nodes,elementsS2,[0.0,10.0,0.0,10.0],[0.0,200.0,100.0,200.0])
+
+    toc = time.clock()
+    print("time for the reading data part:",toc-tic)
+
+    tic0 = time.clock()
+    #############################################################################
+    #      EXPERT PART
+    #############################################################################
+    #      initialisations
+    #############################################################################
+    # get number of nodes, dof and elements from the mesh
+    nnodes = nodes.shape[0]
+    ndof   = nnodes*ndim
+    nelem  = elements.shape[0]
+    print("Number of nodes:",nnodes)
+    print("Number of elements:",nelem)
+
+    # define fixed dof
+    Fixed_Dofs = scipy.hstack([(IdNodesFixed_x-1)*2,(IdNodesFixed_y-1)*2+1])
+
+    # define free dof
+    SolvedDofs = scipy.setdiff1d(range(ndof),Fixed_Dofs)
+
+    # initialize displacement vector
+    Q=scipy.zeros(ndof)
+
+    #############################################################################
+    #      compute stiffness matrix
+    #############################################################################
+    tic = time.clock()
+
+    Ik,Jk,Vk=silex_lib_elt.stiffnessmatrix(nodes,elements,[Young,nu,thickness])
+
+    K=scipy.sparse.csc_matrix( (Vk,(Ik,Jk)), shape=(ndof,ndof) )
+
+    toc = time.clock()
+    print("time to compute the stiffness matrix:",toc-tic)
+
+    #############################################################################
+    #       Solve the problem
+    #############################################################################
+
+    tic = time.clock()
+    #Q[SolvedDofs] = scipy.sparse.linalg.spsolve(K[SolvedDofs,:][:,SolvedDofs],F[SolvedDofs])
+    Q[SolvedDofs] = mumps.spsolve(K[SolvedDofs,:][:,SolvedDofs],F[SolvedDofs])
+    toc = time.clock()
+    print("time to solve the problem:",toc-tic)
+
+    #############################################################################
+    #       compute stress, smooth stress, strain and error
+    #############################################################################
+    tic = time.clock()
+
+    SigmaElem,SigmaNodes,EpsilonElem,EpsilonNodes,ErrorElem,ErrorGlobal=silex_lib_elt.compute_stress_strain_error(nodes,elements,[Young,nu,thickness],Q)
+
+    toc = time.clock()
+    print("time to compute stresses:",toc-tic)
+    print("The global error is:",ErrorGlobal)
+
+    #Elt_max_length=Elt_max_length/1.2
+    local_error=scipy.sqrt(ErrorElem)/scipy.average(scipy.sqrt(ErrorElem))
+    print(scipy.average(scipy.sqrt(ErrorElem)))
+    print(min(local_error),max(local_error))
+    #local_refine = (scipy.sign(local_error-1.0)+1.0)*0.5
+    #local_not_refine = -(scipy.sign(local_error-1.0)-1.0)*0.5
+
+    #local_ratio = local_error*local_refine + local_not_refine
+
+    #NewSize=Elt_max_length/(local_ratio**(2))
+    #NewSize=scipy.sign(Elt_max_length/local_error-1.0)
+    #param = 0.5
+    #NewSize = 0.1+Elt_max_length*2.0/(1+scipy.exp(-param*(1-local_error)))
+    NewSize=scipy.zeros(nelem)
+    nb_refined_elements=0
+
+    for e in range(nelem):
+        idnodes[:] = elements[e,:]-1
+        X=nodes[idnodes,0]
+        Y=nodes[idnodes,1]
+        a23[0,0] = X[0]
+        a23[0,1] = X[1]
+        a23[0,2] = X[2]
+        a23[1,0] = Y[0]
+        a23[1,1] = Y[1]
+        a23[1,2] = Y[2]
+        det_of_sys=silex_lib_elt.det33_ligne_de_un(a23)
+        Area=abs(0.5*det_of_sys)
+        current_size=scipy.sqrt(2.0*Area)
+        if local_error[e]<1.0:
+            NewSize[e]=current_size*2.5
+        else:
+            #NewSize[e]=current_size/1.2
+            NewSize[e]=current_size/(1.2*local_error[e]**(1.0/1.0))
+            nb_refined_elements+=1
+
+    print(nb_refined_elements,nelem)
+
+    if (ErrorGlobal>ErrorGlobalMaxi):
+        fields_to_write=[ [NewSize,'elemental',1,'error'] ]
+        #silex_lib_gmsh.WriteResults('newsize',nodes,elements,eltype,fields_to_write)
+        silex_lib_extra.WritePos('newsize',nodes,elements,NewSize)
+        os.system('gmsh -2 adaptive_mesh.geo')
+        
+
+    #stop    
+    #############################################################################
+    #         Write results to gmsh format
+    #############################################################################
+    tic = time.clock()
+
+    # displacement written on 2 columns:
+    disp=scipy.zeros((nnodes,2))
+    disp[range(nnodes),0]=Q[list(range(0,ndof,2))]
+    disp[range(nnodes),1]=Q[list(range(1,ndof,2))]
+
+    load=scipy.zeros((nnodes,ndim))
+    load[range(nnodes),0]=F[list(range(0,ndof,2))]
+    load[range(nnodes),1]=F[list(range(1,ndof,2))]
+
+    if flag_write_fields==0:
+        fields_to_write=[ [disp,'nodal',ndim,'displacement'],[load,'nodal',ndim,'Force'],
+                          [SigmaElem[range(nelem),[3]],'elemental',1,'Sigma V.M.'],
+                          [SigmaNodes[range(nnodes),[3]],'nodal',1,'Sigma V.M. Smooth'],
+                          [ErrorElem,'elemental',1,'error'],
+                          ]
+
+    if flag_write_fields==1:
+        fields_to_write=[ [disp,'nodal',ndim,'Displacement'],
+                      [load,'nodal',ndim,'Force'],
+                      [SigmaElem[range(nelem),[0]],'elemental',1,'Sigma xx'],
+                      [SigmaElem[range(nelem),[1]],'elemental',1,'Sigma yy'],
+                      [SigmaElem[range(nelem),[2]],'elemental',1,'Sigma xy'],
+                      [SigmaElem[range(nelem),[3]],'elemental',1,'Sigma V.M.'],
+                      [SigmaNodes[range(nnodes),[0]],'nodal',1,'Sigma xx Smooth'],
+                      [SigmaNodes[range(nnodes),[1]],'nodal',1,'Sigma yy Smooth'],
+                      [SigmaNodes[range(nnodes),[2]],'nodal',1,'Sigma xy Smooth'],
+                      [SigmaNodes[range(nnodes),[3]],'nodal',1,'Sigma V.M. Smooth'],
+                      [EpsilonElem[range(nelem),[0]],'elemental',1,'Epsilon xx'],
+                      [EpsilonElem[range(nelem),[1]],'elemental',1,'Epsilon yy'],
+                      [EpsilonElem[range(nelem),[2]]/2.0,'elemental',1,'Epsilon xy'],
+                      [EpsilonNodes[range(nnodes),[0]],'nodal',1,'Epsilon xx Smooth'],
+                      [EpsilonNodes[range(nnodes),[1]],'nodal',1,'Epsilon yy Smooth'],
+                      [EpsilonNodes[range(nnodes),[2]]/2.0,'nodal',1,'Epsilon xy Smooth'],
+                      [ErrorElem,'elemental',1,'Error']
+                      ]
+
+    if flag_write_fields==2:
+        fields_to_write=[  [EpsilonNodes[range(nnodes),[0]],'nodal',1,'Epsilon xx Smooth']
+                      ]
+
+    # write the mesh and the results in a gmsh-format file
+    silex_lib_gmsh.WriteResults(ResultsFileName+'_'+str(Step_number),nodes,elements,eltype,fields_to_write)
+    Step_number=Step_number+1
+
+
+toc = time.clock()
+print("time to write results:",toc-tic)
+print("----- END -----")
+
+
+
+
---
+++ cases/Ice_skate_blade/lame.geo
@@ -0,0 +1,174 @@
+
+h = 10; // average length of the elements
+
+coeff=300/13670; // length about 300 mm
+
+// forward and down
+// Point(1) = {0, 0, 0};
+Point(2) = {0 ,1400*coeff , 0};
+Point(3) = {240*coeff ,1400*coeff , 0};
+Point(4) = {195*coeff ,1225*coeff , 0};
+Point(5) = {320*coeff ,1240*coeff , 0};
+Point(6) = {290*coeff ,1105*coeff , 0};
+Point(7) = {415*coeff ,1115*coeff, 0};
+Point(8) = {395*coeff ,975*coeff , 0};
+Point(9) = {530*coeff,1000*coeff , 0};
+Point(10) = {510*coeff ,770*coeff , 0};
+Point(11) = {705*coeff ,800*coeff , 0};
+Point(12) = {700*coeff ,565*coeff , 0};
+Point(13) = {925*coeff ,625*coeff, 0};
+Point(14) = {915*coeff ,375*coeff , 0};
+Point(15) = {1065*coeff ,470*coeff , 0};
+Point(16) = {1190*coeff,510*coeff , 0};
+Point(17) = {1280*coeff ,505*coeff , 0};
+Point(18) = {1590*coeff ,420*coeff , 0};
+Point(19) = {2040*coeff ,335*coeff, 0};
+Point(20) = {2510*coeff ,275*coeff , 0};
+Point(21) = {3325*coeff ,215*coeff , 0};
+Point(22) = {4090*coeff ,190*coeff , 0};
+Point(23) = {5490*coeff ,170*coeff , 0};
+Point(24) = {6700*coeff ,180*coeff , 0};
+Point(25) = {7945*coeff ,190*coeff, 0};
+Point(26) = {9120*coeff ,200*coeff , 0};
+Point(27) = {10180*coeff ,245*coeff , 0};
+Point(28) = {11050*coeff ,300*coeff , 0};
+Point(29) = {12315*coeff ,375*coeff , 0};
+Point(30) = {12985*coeff ,420*coeff , 0};
+Point(31) = {13565*coeff ,475*coeff, 0};
+Point(32) = {13668*coeff ,485*coeff , 0};
+Point(33) = {13670*coeff ,545*coeff , 0};
+Point(34) = {13665*coeff ,840*coeff , 0};
+Point(35) = {13645*coeff ,930*coeff , 0};
+Point(36) = {13595*coeff ,1000*coeff , 0};
+Point(37) = {13550*coeff ,1050*coeff, 0};
+Point(38) = {13485*coeff ,1090*coeff , 0};
+Point(39) = {13435*coeff ,1115*coeff , 0};
+Point(40) = {13360*coeff ,1135*coeff , 0};
+Point(41) = {13305*coeff ,1140*coeff , 0};
+Point(42) = {13170*coeff ,1150*coeff , 0};
+Point(43) = {13010*coeff ,1160*coeff, 0};
+Point(44) = {12880*coeff ,1165*coeff , 0};
+Point(45) = {12705*coeff ,1185*coeff , 0};
+Point(46) = {12570*coeff ,1212*coeff , 0};
+Point(47) = {12445*coeff ,1250*coeff , 0};
+Point(48) = {12285*coeff ,1310*coeff , 0};
+Point(49) = {12145*coeff ,1380*coeff, 0};
+Point(50) = {12055*coeff ,1456*coeff , 0};
+Point(51) = {11980*coeff ,1532*coeff , 0};
+Point(52) = {11895*coeff ,1635*coeff , 0};
+Point(53) = {11840*coeff ,1760*coeff , 0};
+Point(54) = {11805*coeff ,2035*coeff , 0};
+Point(55) = {11800*coeff ,2460*coeff, 0};
+
+// up part 1
+Point(56) = {10735*coeff ,2460*coeff , 0};
+Point(57) = {10795*coeff ,2215*coeff , 0};
+Point(58) = {10825*coeff ,2040*coeff , 0};
+Point(59) = {10830*coeff ,1880*coeff , 0};
+Point(60) = {10810*coeff ,1735*coeff , 0};
+Point(61) = {10760*coeff ,1630*coeff, 0};
+Point(62) = {10685*coeff ,1538*coeff , 0};
+Point(63) = {10585*coeff ,1440*coeff , 0};
+Point(64) = {10455*coeff ,1350*coeff , 0};
+Point(65) = {10355*coeff ,1295*coeff , 0};
+Point(66) = {10215*coeff ,1250*coeff , 0};
+Point(67) = {9945*coeff ,1210*coeff, 0};
+Point(68) = {9625*coeff ,1180*coeff , 0};
+Point(69) = {9120*coeff ,1155*coeff , 0};
+Point(70) = {8215*coeff ,1150*coeff , 0};
+Point(71) = {7020*coeff ,1135*coeff , 0};
+Point(72) = {6370*coeff ,1140*coeff , 0};
+Point(73) = {6050*coeff ,1155*coeff, 0};
+Point(74) = {5865*coeff ,1170*coeff , 0};
+Point(75) = {5700*coeff ,1200*coeff , 0};
+Point(76) = {5555*coeff ,1245*coeff , 0};
+Point(77) = {5450*coeff ,1305*coeff , 0};
+Point(78) = {5360*coeff ,1387*coeff , 0};
+Point(79) = {5285*coeff ,1470*coeff, 0};
+Point(80) = {5210*coeff ,1585*coeff , 0};
+Point(81) = {5160*coeff ,1695*coeff , 0};
+Point(82) = {5144*coeff ,1810*coeff , 0};
+Point(83) = {5145*coeff ,1955*coeff , 0};
+Point(84) = {5165*coeff ,2125*coeff , 0};
+Point(85) = {5206*coeff ,2290*coeff, 0};
+Point(86) = {5250*coeff ,2460*coeff, 0};
+
+// up part 2
+Point(87) = {4035*coeff ,2460*coeff , 0};
+Point(88) = {4130*coeff ,2135*coeff  , 0};
+Point(89) = {4175*coeff ,1950*coeff  , 0};
+Point(90) = {4185*coeff ,1820*coeff  , 0};
+Point(91) = {4170*coeff ,1665*coeff  , 0};
+Point(92) = {4135*coeff ,1525*coeff , 0};
+Point(93) = {4050*coeff ,1390*coeff  , 0};
+Point(94) = {3935*coeff ,1280*coeff  , 0};
+Point(95) = {3815*coeff ,1215*coeff  , 0};
+Point(96) = {3660*coeff ,1180*coeff  , 0};
+Point(97) = {3475*coeff ,1155*coeff  , 0};
+Point(98) = {3315*coeff ,1150*coeff , 0};
+Point(99) = {2965*coeff ,1140*coeff  , 0};
+Point(100) = {2485*coeff ,1140*coeff  , 0};
+Point(101) = {2095*coeff ,1155*coeff  , 0};
+Point(102) = {1870*coeff ,1180*coeff  , 0};
+Point(103) = {1670*coeff ,1215*coeff  , 0};
+Point(104) = {1525*coeff ,1265*coeff , 0};
+Point(105) = {1425*coeff ,1315*coeff  , 0};
+Point(106) = {1280*coeff ,1420*coeff  , 0};
+Point(107) = {1165*coeff ,1545*coeff  , 0};
+Point(108) = {1095*coeff ,1660*coeff  , 0};
+Point(109) = {1054*coeff ,1799*coeff  , 0};
+Point(110) = {1045*coeff ,1915*coeff , 0};
+Point(111) = {1054*coeff ,2009*coeff  , 0};
+Point(112) = {1076*coeff ,2076*coeff  , 0};
+Point(113) = {1132*coeff,2166*coeff  , 0};
+Point(114) = {1219*coeff ,2260*coeff  , 0};
+Point(115) = {1300*coeff ,2335*coeff  , 0};
+Point(116) = {1355*coeff ,2395*coeff , 0};
+Point(117) = {1390*coeff ,2460*coeff , 0};
+
+// up forward
+Point(118) = {600*coeff ,2460*coeff , 0};
+Point(119) = {555*coeff ,2418*coeff , 0};
+Point(120) = {476*coeff ,2268*coeff , 0};
+Point(121) = {390*coeff ,2092*coeff , 0};
+Point(122) = {330*coeff ,1923*coeff , 0};
+Point(123) = {262*coeff ,1781*coeff, 0};
+Point(124) = {150*coeff ,1589*coeff , 0};
+Point(125) = {65*coeff ,1473*coeff, 0};
+
+
+Line(1) = {2, 3};
+Line(2) = {3, 4};
+Line(3) = {4, 5};
+Line(4) = {5, 6};
+Line(5) = {6, 7};
+Line(6) = {7, 8};
+Line(7) = {8, 9};
+Line(8) = {9,10};
+Line(9) = {10,11};
+Line(10) = {11,12};
+Line(11) = {12,13};
+Line(12) = {13,14};
+Line(55) = {55,56};
+Line(86) = {86, 87};
+Line(117) = {117, 118};
+
+
+Spline(126) = {14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
+Spline(127) = {23, 24, 25, 26, 27, 28};
+Spline(128) = {28, 29, 30, 31, 32};
+Spline(129) = {32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55};
+Spline(130) = {56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86};
+Spline(131) = {87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117};
+Spline(132) = {118, 119, 120, 121, 122, 123, 124, 125, 2};
+
+
+Line Loop(135) = {117, 132, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 126, 127, 128, 129, 55, 130, 86, 131};
+Plane Surface(136) = {135};
+
+Characteristic Length {94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57} = h;
+
+Physical Line(1) = {117, 86, 55};
+Physical Line(2) = {127, 126};
+Physical Surface(3) = {136};
+
